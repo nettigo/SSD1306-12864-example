c6306ed201ca1d83d78a4f32ef43e386dab7969f
netmaniac <rugowski@nhw.pl>
2020-08-22 00:17:01 +0200

    test na Wemosa (wymaga uwspólnienia z Arduino -> Wire.begin()) Nettigowy tekst

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -246,13 +246,33 @@ void testdrawchar(void) {
   delay(2000);
 }
 
+void testNtgText(void) {
+    display.clearDisplay();
+
+    display.setTextSize(1);             // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE);        // Draw white text
+    display.setCursor(0,0);             // Start at top-left corner
+    display.println(F("Ahoj przygodo!"));
+    display.setCursor(0,10);             // Start at top-left corner
+    display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+    display.print(F("Kto ananasowy ma dom?"));
+
+    display.setCursor(0,20);             // Start at top-left corner
+    display.setTextSize(2,1);             // Draw 2X-scale text
+    display.setTextColor(SSD1306_WHITE);
+//    display.println("");
+    display.println("Nettigo!");
+    display.display();
+    delay(2000);
+}
+
 void testdrawstyles(void) {
   display.clearDisplay();
 
   display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);        // Draw white text
   display.setCursor(0,0);             // Start at top-left corner
-  display.println(F("Hello, world!"));
+  display.println(F("Ahoj przygodo!"));
 
   display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
   display.println(3.141592);
@@ -271,7 +291,7 @@ void testscrolltext(void) {
   display.setTextSize(2); // Draw 2X-scale text
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(10, 0);
-  display.println(F("scroll"));
+  display.println(F("Nettigo"));
   display.display();      // Show initial text
   delay(100);
 
@@ -349,7 +369,7 @@ void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
 }
 void setup() {
   Serial.begin(9600);
-
+  Wire.begin(D3, D4);
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
@@ -358,7 +378,7 @@ void setup() {
 
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
-  display.display();
+  testNtgText();
   delay(2000); // Pause for 2 seconds
 
   // Clear the buffer
